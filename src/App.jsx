de6a51a685f4548a454f7e1b33a0526c7ad082b8
Dan <rodger.swan@gmail.com>
2024-01-04 14:01:36 +1100

    in app.jsx set up with outline structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Dashboard from './components/Dashboard'
 
 function App() {
 
   return (
     <div className="app">
       <main>
-      <img
-          src="/starwars.jpeg"
+      {/* <img
+          src="/public/026-dvd2_star_wars_poster_classic_1_web.jpg"
           alt="starwars-poster-img"
           className="bg-img"
-        /> 
+        />  */}
+        <Dashboard/>
       </main>
     </div>
   )
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './dashboard.module.css'
+
+export default function Dashboard() {
+  
+
+  return (
+    <div className={styles.dashboardContainer}>
+      <h1>STARWARS</h1>
+    </div>
+  )
+}
