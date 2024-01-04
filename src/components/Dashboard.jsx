d0a3fedf4e489cf8494446b5b17e6c5a2600e929
Dan <rodger.swan@gmail.com>
2024-01-04 14:13:45 +1100

    set up questions in dashboard and test

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,10 +3,18 @@ import styles from './dashboard.module.css'
 
 export default function Dashboard() {
   
+  const questions = [
+    'people', 'films', 'species'
+  ]
 
   return (
     <div className={styles.dashboardContainer}>
       <h1>STARWARS</h1>
+      {questions.map((question, index) => {
+        return (
+          <button key={index}>{ question}</button>
+)
+      })}
     </div>
   )
 }
